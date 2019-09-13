ffadafba314711e4a8fcf19c939b6a5d4dd02863
Erick Maria <32113867+ErickMaria@users.noreply.github.com>
2019-09-13 16:42:35 -0300

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -2,3 +2,4 @@
 LOCAL_IP=`ip route get 1.1.1.1 | grep -oP 'src \K\S+'`
 RANCHER_SERVER_URL=$1
 docker run -e "CATTLE_AGENT_IP=$LOCAL_IP" --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.2.11 $RANCHER_SERVER_URL
+ASAS
