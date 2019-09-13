db1a34eb7881f2be7d878212b10bd4413d78a8f3
Erick Maria <32113867+ErickMaria@users.noreply.github.com>
2019-09-13 16:39:52 -0300

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-touch /home/adm_conductor/myextensionscript.txt
-echo "hello world..." > /home/adm_conductor/hello.txt
+LOCAL_IP=`ip route get 1.1.1.1 | grep -oP 'src \K\S+'`
+RANCHER_SERVER_URL=$LOCAL_IP
+docker run -e "CATTLE_AGENT_IP=$LOCAL_IP" --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.2.11 $RANCHER_SERVER_URL
