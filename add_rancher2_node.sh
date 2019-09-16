022d701e7fa76f326377742db335ec19f90a132d
Erick Maria <32113867+ErickMaria@users.noreply.github.com>
2019-09-16 16:02:03 -0300

    Update add_rancher2_node.sh

--- add_rancher2_node.sh
+++ add_rancher2_node.sh
@@ -3,4 +3,4 @@ LOCAL_IP=`ip route get 1.1.1.1 | grep -oP 'src \K\S+'`
 RANCHER_SERVER_URL=$1
 RANCHER_TOKEN=$2
 RANCHER_CHECKSUM=$3
-sudo docker run -d --privileged --restart=unless-stopped --net=host -v /etc/kubernetes:/etc/kubernetes -v /var/run:/var/run rancher/rancher-agent:v2.3.0-rc2 --server $RANCHER_SERVER_URL --token RANCHER_TOKEN --ca-checksum $RANCHER_CHECKSUM --internal-address $LOCAL_IP --etcd --controlplane --worker
+echo sudo docker run -d --privileged --restart=unless-stopped --net=host -v /etc/kubernetes:/etc/kubernetes -v /var/run:/var/run rancher/rancher-agent:v2.3.0-rc2 --server $RANCHER_SERVER_URL --token $RANCHER_TOKEN --ca-checksum $RANCHER_CHECKSUM --internal-address $LOCAL_IP --etcd --controlplane --worker
