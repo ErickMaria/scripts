793e5dbac7b29bbe25778117ead20c0f112c3543
Erick Maria <32113867+ErickMaria@users.noreply.github.com>
2019-11-03 13:21:57 -0300

    Update consecutive_requests.sh

--- consecutive_requests.sh
+++ consecutive_requests.sh
@@ -1,27 +1,27 @@
-  #!bin/bash
+#!bin/bash
 
 rqtt=1320
 total_resquest_sucecess=0
 
 while [ $total_resquest_sucecess != $rqtt ]
 do
-  baseline=0
+  current=0
   canary=0
-  get_versions=$(siege -p -r100 -d1 -c10 http://lb-helloapp.35.196.211.203.xip.io | awk '{ print $5 }' )
+  get_versions=$(siege -p -r110 -d1 -c12 http://lb-helloapp.35.196.211.203.xip.io | awk '{ print $5 }' )
   for version in $get_versions
   do
       if [ $version == "1.0.0" ]
       then
-        ((baseline++))
+        ((current++))
       fi     
       if [ $version == "2.0.0" ]
       then
         ((canary++))
       fi
   done
-  total_resquest_sucecess=$((baseline+canary))
+  total_resquest_sucecess=$((current+canary))
 done
 
 echo "total requests => $total_resquest_sucecess"
-echo "production (v1.0.0) => $baseline"
-echo "canary     (v2.0.0) => $canary"
+echo "current (v1.0.0) => $current"
+echo "canary  (v2.0.0) => $canary"
